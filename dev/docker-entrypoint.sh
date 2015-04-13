10a1fe28037157fcad3ac1bbf36f3c392caea9c6
MartinRumanek <martin@rumanek.cz>
2015-04-13 21:29:16 +0200

    password from environment

--- dev/docker-entrypoint.sh
+++ dev/docker-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-xmlstarlet ed --inplace -u //config/hostname -v mysql -u //config/user -v koha_knihovna -u //config/pass -v tajneHeslo /etc/koha/sites/knihovna/koha-conf.xml 
+xmlstarlet ed --inplace -u //config/hostname -v mysql -u //config/user -v koha_knihovna -u //config/pass -v $KOHA_PASSWORD /etc/koha/sites/knihovna/koha-conf.xml 
 
 #TODO: wait for db start...better
 sleep 15
-koha_db_count=`echo "use koha_knihovna; show tables;" | mysql --host=mysql --user=root --password="tajneHeslo" | wc -l`
+koha_db_count=`echo "use koha_knihovna; show tables;" | mysql --host=mysql --user=root --password="$KOHA_PASSWORD" | wc -l`
 
 if [ "$koha_db_count" -lt 1 ]
 then
    echo "Koha database is missing"
-   mysql  -p koha_knihovna --user=koha_knihovna --host=mysql --password="tajneHeslo" < /import.sql
+   mysql  -p koha_knihovna --user=koha_knihovna --host=mysql --password=$KOHA_PASSWORD < /import.sql
    sed 's/ServerName .*//' /etc/apache2/sites-enabled/knihovna.conf > knihovna.conf && \
    mv knihovna.conf /etc/apache2/sites-enabled/knihovna.conf
 fi
